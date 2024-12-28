345ae94d57a48c89efe99a87dbe215854592f1e1
kimsoyoung <intosy227@gmail.com>
2024-12-28 22:34:02 +0900

    html 띄우기
    - webConfig 파일에 ResourceResolver를 상속해서 templates 폴더도 static 폴더로 추가함.

---
+++ src/main/java/com/dev/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.dev.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/")
+	public String main(Model model) {
+		model.addAttribute("data", "data1");
+		return "index.html";
+	}
+	
+	@GetMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("data", "data2");
+		return "home.html";
+	}
+
+}
